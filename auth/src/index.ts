3d5b15cadcabba3a0dd4669c8ee0ba7bc438acce
Emre Çolak <emrecolak@Emre.home>
2023-03-14 22:38:52 +0300

    ingress-nginx setup

--- auth/src/index.ts
+++ auth/src/index.ts
@@ -5,6 +5,10 @@ const app = express()
 
 app.use(express.json())
 
+app.get('/api/users/currentUser', (req, res) => {
+    res.send('Hi there')
+})
+
 app.listen(3000, () => {
     console.log('Listening on port 3000!!!')
 })
