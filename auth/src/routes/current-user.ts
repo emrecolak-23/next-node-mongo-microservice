adb8c8274ab464c707549aa00d70540b55ac7e67
Emre Çolak <emrecolak@Emre.home>
2023-03-18 19:42:00 +0300

    requiering auth for route access

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,7 +2,9 @@ import express from 'express'
 import { currentUser } from '../middlewares/current-user'
 const router = express.Router()
 
-router.get('/api/users/currentuser', currentUser,(req, res) => {
+import { requireAuth } from '../middlewares/require-auth'
+
+router.get('/api/users/currentuser', currentUser, requireAuth,(req, res) => {
     res.send({currentUser: req.currentUser || null})
 })
 
