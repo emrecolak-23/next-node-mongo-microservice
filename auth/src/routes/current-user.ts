35c011437d9a874711608b0aa3674c5d8fe67b95
Emre Çolak <emrecolak@Emre.home>
2023-03-18 19:44:25 +0300

    test require auth middleware

--- auth/src/routes/current-user.ts
+++ auth/src/routes/current-user.ts
@@ -2,9 +2,8 @@ import express from 'express'
 import { currentUser } from '../middlewares/current-user'
 const router = express.Router()
 
-import { requireAuth } from '../middlewares/require-auth'
 
-router.get('/api/users/currentuser', currentUser, requireAuth,(req, res) => {
+router.get('/api/users/currentuser', currentUser,(req, res) => {
     res.send({currentUser: req.currentUser || null})
 })
 
