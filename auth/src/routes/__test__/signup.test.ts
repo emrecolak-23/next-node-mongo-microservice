b2827433b4026cf4c3a770cda61282357a3df7c9
Emre Çolak <emrecolak@Emre.home>
2023-03-19 13:36:04 +0300

    testing invalid inputs

--- auth/src/routes/__test__/signup.test.ts
+++ auth/src/routes/__test__/signup.test.ts
@@ -11,3 +11,39 @@ it('return a 201 on successful signup', async () => {
             })
             .expect(201)
 })
+
+it('returns a 400 with an invalid email', async () => {
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'asasklaksa',
+            password: 'pass123'
+        })
+        .expect(400)
+})
+
+it('returns a 400 with an invalid password', async () =>{
+    return request(app)
+        .post('/api/users/signup')
+        .send({
+            email: 'colakkemre@gmail.com',
+            password: ''
+        })
+        .expect(400)
+})
+
+it('return a 400 with missing email and password', async () => {
+    await request(app)
+            .post('/api/users/signup')
+            .send({
+                email: 'colakkemre@gmail.com'
+            })
+            .expect(400)
+    
+    await request(app)
+            .post('/api/users/signup')
+            .send({
+                password: 'pass123'
+            })
+            .expect(400)
+})
