16eaee91aeae17f02342e3397cb52a1dd88fe02c
Emre Çolak <emrecolak@Emre.home>
2023-03-19 19:53:40 +0300

    overview getInitialProps on ssr

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,5 +1,12 @@
-const Landing = () => {
+const LandingPage = ({ color }) => {
+  console.log('I am in the component', color);
   return <div>Landing23</div>;
 };
 
-export default Landing;
+LandingPage.getInitialProps = () => {
+  console.log('I am on the server');
+
+  return { color: 'red' };
+};
+
+export default LandingPage;
