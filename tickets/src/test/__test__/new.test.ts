3695048a22952e14395652071dba61bc4f37b459
Emre Çolak <emrecolak@Emre.home>
2023-03-21 21:51:33 +0300

    create a test cases for tickets service

---
+++ tickets/src/test/__test__/new.test.ts
@@ -0,0 +1,22 @@
+import request from 'supertest'
+import { app } from '../../app'
+
+it('has a route handler listening to /api/tickets for posts request', async () => {
+
+})
+
+it('can only be access if the user is signed in', async () => {
+
+})
+
+it('returns an error if an invalid title is provided', () => {
+
+})
+
+it('returns an error if an invalid price is provided', async () => {
+
+})
+
+it('creates a ticket with valid inputs', async () => {
+
+})
