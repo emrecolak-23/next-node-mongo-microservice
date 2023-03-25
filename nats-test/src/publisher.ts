0221450111ea9d4537fb558367c0e3beafdb3b5e
Emre Çolak <emrecolak@Emre.home>
2023-03-25 15:04:01 +0300

    updating common module

--- nats-test/src/publisher.ts
+++ nats-test/src/publisher.ts
@@ -7,13 +7,17 @@ const stan = nats.connect('ticketing', 'abc', {
     url: 'http://localhost:4222'
 })
 
-stan.on('connect', () => {
+stan.on('connect', async () => {
     console.log('Publisher connected to nats')
 
     const publisher = new TicketCreatedPublisher(stan)
-    publisher.publish({
-        id: '123',
-        title: 'New York Fashion Week',
-        price: 20
-    })
+    try{
+        await publisher.publish({
+            id: '123',
+            title: 'New York Fashion Week',
+            price: 20
+        })
+    } catch(err) {
+        console.error(err)
+    }
 })
