f42a91316f8d3105f04a493eb9e0b9d6f6fe9596
Emre Çolak <emrecolak@Emre.home>
2023-03-26 16:31:18 +0300

    test suite setup in orders service

--- orders/src/routes/new.ts
+++ orders/src/routes/new.ts
@@ -4,8 +4,9 @@ import { BadRequestError, NotFoundError, OrderStatus, requireAuth, validateReque
 import { body } from 'express-validator'
 import { Ticket } from '../models/ticket'
 import { Order } from '../models/order'
-
 const router = express.Router()
+
+const EXPIRATION_WINDOW_SECONDS = 15 * 60
 
 router.post('/api/orders', requireAuth, [
     body('ticketId')
@@ -30,13 +31,20 @@ router.post('/api/orders', requireAuth, [
     }
 
     // Calculate an expiration date for this order
-
+    const expiration = new Date()
+    expiration.setSeconds(expiration.getSeconds() + EXPIRATION_WINDOW_SECONDS)
+    
     // Build the order and save it to the databse
-
+    const order = Order.build({
+        userId: req.currentUser!.id,
+        status: OrderStatus.Created,
+        expiresAt: expiration,
+        ticket: ticket
+    })
+    await order.save()
     // Publish an event saying that an order was created
     
-    
-    res.send({})
+    res.status(201).send(order)
 })
 
 export { router as newOrderRouter}
