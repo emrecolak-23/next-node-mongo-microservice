670145b9ef8e842e23bec9aeb25b02ad89ab3faa
Emre Çolak <emrecolak@Emre.home>
2023-04-01 15:49:04 +0300

    ticket-update-publisher added into order-created-listener, stan client changes from private to protected in base listener and publisher classes, orderId added into ticket-updated-event

--- tickets/src/events/publishers/ticket-updated-publisher.ts
+++ tickets/src/events/publishers/ticket-updated-publisher.ts
@@ -3,3 +3,4 @@ import { Publisher, Subjects, TicketUpdatedEvent } from "@emticketsapp/common";
 export class TicketUpdatedPublisher extends Publisher<TicketUpdatedEvent> {
     subject: Subjects.TicketUpdated = Subjects.TicketUpdated
 }
+
