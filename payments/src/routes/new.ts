f0e412b4552cf206d2e410e42be1ad164b76e829
Emre Çolak <emrecolak@Emre.home>
2023-04-04 20:52:26 +0300

    create payment model and testing payment creation

--- payments/src/routes/new.ts
+++ payments/src/routes/new.ts
@@ -3,6 +3,7 @@ import { body } from 'express-validator'
 import { requireAuth, validateRequest, BadRequestError, NotFoundError, NotAuthorizedError, OrderStatus } from '@emticketsapp/common'
 import { Order } from '../models/order'
 import { stripe } from '../stripe'
+import { Payment } from '../models/payment'
 
 const router = express.Router()
 
@@ -32,12 +33,19 @@ router.post('/api/payments', requireAuth,
     }
 
 
-    await stripe.charges.create({
+    const charge = await stripe.charges.create({
         currency: 'usd',
         amount: order.price * 100,
         source: token
     })
 
+    const payment = Payment.build({
+        orderId,
+        stripeId: charge.id
+    }) 
+
+    await payment.save()
+    
     res.status(201).send({success: true})
 
 })
